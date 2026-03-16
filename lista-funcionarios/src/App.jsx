4d1eb1afaf1c56bb61fa568c08d8fc1f6231ca06
Maria Clara <maria.c.ferreira47@aluno.senai.br>
2026-03-16 16:24:46 -0300

    Feat: Adiciona atividade de card sobre lista de funcionario

---
+++ lista-funcionarios/src/App.jsx
@@ -0,0 +1,44 @@
+
+
+function App() {
+  const funcionarios = [
+     { id: 1, nome: "Ana Silva",     
+      cargo: "Desenvolvedora Frontend",
+      dataAdmissao: "10/02/2023",
+      foto: "https://randomuser.me/api/portraits/women/44.jpg" 
+    },
+
+    { id: 2, nome: "Carlos Eduardo", 
+      cargo: "Engenheiro de Software",  
+      dataAdmissao: "15/08/2022", 
+      foto: "https://randomuser.me/api/portraits/men/32.jpg"  
+    },
+    { id: 3, 
+      nome: "Mariana Souza",  
+      cargo: "Designer UI/UX",          
+      dataAdmissao: "05/11/2021", 
+      foto: "https://randomuser.me/api/portraits/women/68.jpg"
+     },
+    { id: 4,
+      nome: "Rafael Lima",    
+      cargo: "Engenheiro DevOps",       
+      dataAdmissao: "12/01/2021", 
+      foto: "https://randomuser.me/api/portraits/men/22.jpg"  
+    },
+    { id: 5, 
+      nome: "Juliana Costa",  
+      cargo: "Gerente de Projetos",     
+      dataAdmissao: "20/03/2019", 
+      foto: "https://randomuser.me/api/portraits/women/33.jpg"
+    } 
+
+  ]
+
+  return (
+    <>
+     
+    </>
+  )
+}
+
+export default App
